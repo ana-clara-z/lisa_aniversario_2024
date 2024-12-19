7dda08bb7c9803839ba2cb9c4aadf51dc6983bfe
ana-clara-z <anazimmermann.2014@aluno.pmf.sc.gov.br>
2024-12-18 21:53:04 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,194 @@
+const img = document.querySelector("#img-lisa");
+
+let graus = -15;
+img.style.transform = 'rotate(' + graus + 'deg)'; 
+
+setInterval(function() {
+    if (graus === -15) {
+        graus = 15;
+    } else {
+        graus = -15;
+    }
+    img.style.transform = 'rotate(' + graus + 'deg)';    
+}, 2000);
+
+
+
+const img2 = document.querySelector("#dandypelucia");
+
+let graus2  = 0;
+
+function girardandy() {
+    graus2 = graus2 + 90;
+
+    if(graus2 >= 450) graus2 = 0;
+
+    img2.style.transform = 'rotate(' + graus2 + 'deg)'; 
+
+}
+
+
+
+const img3 = document.querySelector("#razzle");
+
+let graus3  = 0;
+
+function girarazzle() {
+    graus3 = graus3 + 90;
+
+    if(graus3 >= 450) graus3 = 0;
+
+    img3.style.transform = 'rotate(' + graus3 + 'deg)'; 
+
+}
+
+
+
+const img4 = document.querySelector("#shelly");
+
+let graus4  = 0;
+
+function girashelly() {
+    graus4 = graus4 + 90;
+
+    if(graus4 >= 450) graus4 = 0;
+
+    img4.style.transform = 'rotate(' + graus4 + 'deg)'; 
+
+}
+
+
+
+const img5 = document.querySelector("#pebble");
+
+let graus5  = 0;
+
+function girapebble() {
+    graus5 = graus5 + 90;
+
+    if(graus5 >= 450) graus5 = 0;
+
+    img5.style.transform = 'rotate(' + graus5 + 'deg)'; 
+
+}
+
+
+
+const img6 = document.querySelector("#astro");
+
+let graus6  = 0;
+
+function giraastro() {
+    graus6 = graus6 + 90;
+
+    if(graus6 >= 450) graus6 = 0;
+
+    img6.style.transform = 'rotate(' + graus6 + 'deg)'; 
+
+}
+
+const img7 = document.querySelector("#sprout");
+
+let graus7  = 0;
+
+function girasprout() {
+    graus7 = graus7 + 90;
+
+    if(graus7 >= 450) graus7 = 0;
+
+    img7.style.transform = 'rotate(' + graus7 + 'deg)'; 
+
+}
+
+
+
+const img8 = document.querySelector("#cosmo");
+
+let graus8  = 0;
+
+function giracosmo() {
+    graus8 = graus8 + 90;
+
+    if(graus8 >= 450) graus8 = 0;
+
+    img8.style.transform = 'rotate(' + graus8 + 'deg)'; 
+
+}
+
+
+
+
+const img9 = document.querySelector("#shrimpo");
+
+let graus9  = 0;
+
+function girashrimpo() {
+    graus9 = graus9 + 90;
+
+    if(graus9 >= 450) graus9 = 0;
+
+    img9.style.transform = 'rotate(' + graus9 + 'deg)'; 
+
+}
+
+
+
+const img10 = document.querySelector("#poppy");
+
+let graus10  = 0;
+
+function girapoppy() {
+    graus10 = graus10 + 90;
+
+    if(graus10 >= 450) graus10 = 0;
+
+    img10.style.transform = 'rotate(' + graus10 + 'deg)'; 
+
+}
+
+
+
+
+const img11 = document.querySelector("#boxten");
+
+let graus11  = 0;
+
+function giraboxten() {
+    graus11 = graus11 + 90;
+
+    if(graus11 >= 450) graus11 = 0;
+
+    img11.style.transform = 'rotate(' + graus11 + 'deg)'; 
+
+}
+
+
+
+const img12 = document.querySelector("#toddles");
+
+let graus12  = 0;
+
+function giratoddles() {
+    graus12 = graus12 + 90;
+
+    if(graus12 >= 450) graus12 = 0;
+
+    img12.style.transform = 'rotate(' + graus12 + 'deg)'; 
+
+}
+
+
+
+
+const img13 = document.querySelector("#rodger");
+
+let graus13  = 0;
+
+function girarodger() {
+    graus13 = graus13 + 90;
+
+    if(graus13 >= 450) graus13 = 0;
+
+    img13.style.transform = 'rotate(' + graus13 + 'deg)'; 
+
+}
